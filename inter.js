97765b9e4bbc469e9f95e281807423947e2bfac7
Bernard <84543016+Artosh-afr@users.noreply.github.com>
2022-02-17 15:49:42 +0200

    Update inter.js

--- inter.js
+++ inter.js
@@ -17,10 +17,10 @@ function checker(){
 for(let i=0;i<play.length;i++){
     if (yourBet==play[i]){
        return output=
-            `Your play: ${yourBet} \n My play: ${play[gen]}`;
+       `Your play: ${yourBet} \n My play: ${play[gen]}`;
     
     }
-}return output="tik dit reg in";
+}return output="Tik dit reg in";
 }
 
 
@@ -30,21 +30,55 @@ let result="";
 
 
 function doener(){
-if(output==="tik dit reg in"){
-     return  result="geen result nie";
-}else if(yourBet===play[gen]){
-     return  result="Tied";
-}else if(yourBet==="Paper" && play[gen]==="Rock"){
-     return  result="U win";
-}else if(yourBet==="Rock" && play[gen]==="Scissors"){
-     return  result="U win";
-}else if(yourBet==="Scissors" && play[gen]==="Paper"){
-     return  result="U win";
-}else{  return result="I win :P"}
-}
+     if(output==="Tik dit reg in"){
+          return  result="geen result nie";
+     }else if(yourBet===play[gen]){
+          return  result="Tied";
+     }else if(yourBet==="Paper" && play[gen]==="Rock"){
+          return  result="U win";
+     }else if(yourBet==="Rock" && play[gen]==="Scissors"){
+          return  result="U win";
+     }else if(yourBet==="Scissors" && play[gen]==="Paper"){
+          return  result="U win";
+     }else{  return result="I win :P"}
+     }
+
+ let  compcounter=0;
+ let yourcounter=0;
+ function yourCounter(){
+      if(output==="Tik dit reg in"){
+          yourcounter+=0;
+      }else if(yourBet===play[gen]){
+           yourcounter+=0;
+      }else if(yourBet==="Paper"  && play[gen]==="Rock"){
+           yourcounter+=1;
+      }else if(yourBet==="Rock" && play[gen]==="Scissors"){
+           yourcounter+=1;
+      }else if(yourBet==="Scissors" && play[gen]==="Paper"){
+          yourcounter+=1;
+      }else{  return  yourcounter+=0;}
+      return yourcounter;
+      }
+
+
+ function compCounter(){
+      if(output==="Tik dit reg in"){
+     }else if(yourBet===play[gen]){
+        compcounter+=0;
+    }else if(yourBet==="Paper"  && play[gen]==="Rock"){
+         compcounter+=0;
+     }else if(yourBet==="Rock" && play[gen]==="Scissors"){
+          compcounter+=0;
+     }else if(yourBet==="Scissors" && play[gen]==="Paper"){
+         compcounter+=0;
+     }else{  return  compcounter+=1;}
+      return compcounter;
+      }
 
 function returner(){
-    document.getElementById("outputer").innerHTML=checker()
-    document.getElementById("result").innerHTML=doener();
-    gen=Math.round(Math.random()*2);
-}
+     document.getElementById("outputer").innerHTML=checker()
+     document.getElementById("result").innerHTML=doener();
+     document.getElementById("Compcounter").innerHTML=compCounter();
+     document.getElementById("Yourscore").innerHTML= yourCounter();
+     gen=Math.round(Math.random()*2);
+ }
